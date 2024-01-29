e209a65aa164acaf188d89c257e8f128ee7257c7
rishimalhan <rmalhan0112@gmail.com>
2024-01-28 21:32:32 -0500

    reverting

--- void_pointer_ws/src/speech_to_text/setup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3.8
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup()
-d["packages"] = ["speech_to_text"]
-d["package_dir"] = {"": "src"}
-setup(**d)
